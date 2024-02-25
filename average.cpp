49b802fe41b702f24e00b4b0663dac4cc352241f
Your Name <you@example.com>
2024-02-25 19:41:07 +0700

    done

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main(int avgc, char* argv[]){
+
+    int N = avgc - 1;
+    float sum = 0;
+
+ if(avgc == 1){
+    cout << "Please input numbers to find average." << endl;
+ }else{
+ 
+    for(int i = 0; i < avgc-1; i++){
+        sum += atof(argv[i+1]);
+    }
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << N << " numbers = " << sum/N << endl;
+        cout << "---------------------------------" << endl;
+}
+
+}
